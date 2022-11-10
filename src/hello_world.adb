66c93fa461b8506786ba2e30f90da04b6845604d
Sergio García <sgarciac@indra.es>
2022-11-10 14:23:09 +0100

    Modify Hello_World

--- src/hello_world.adb
+++ src/hello_world.adb
@@ -1,6 +1,22 @@
 with Ada.Text_IO;
 
 procedure Hola_Mundo is
+
+   V_ENTERO_DE_COMPROBACION : INTEGER;
+
 begin
-   Ada.Text_IO.Put_Line("¡Hola, mundo!");
+   
+   V_ENTERO_DE_COMPROBACION := 1;
+
+   if (V_ENTERO_DE_COMPROBACION = 1) then
+   
+      Ada.Text_IO.Put_Line("¡Hola, mundo!");
+
+   else
+
+      Ada.Text_IO.Put_Line("Ha habido un error");
+
+   end if;
+
+
 end Hola_Mundo;
